ac18b3abd834dd1e988b7e746ab6ed38f9891298
FVO87 <140124985+FVO87@users.noreply.github.com>
2024-07-26 12:06:38 -0300

    Create utm_whatsapp.js

---
+++ utm_whatsapp.js
@@ -0,0 +1,58 @@
+function updateWhatsAppLinks(clientId) {
+    const pageTitle = document.title;
+    const message = "Oi, estava vendo a pÃ¡gina: " + pageTitle + " - e gostaria de saber mais (ID: #" + clientId + ")";
+    const encodedMessage = encodeURIComponent(message);
+	console.log("encodedMessage ======>",encodedMessage);
+    const links = document.querySelectorAll('a[href*="api.whatsapp.com"], a[href*="wa.me"]');
+    console.log('links ----->', links);
+    links.forEach(link => {
+      let href = link.getAttribute('href');
+      if (href) {
+        if (href.includes('?')) {
+          href += "&text=" + encodedMessage;
+        } else {
+          href += "?text=" + encodedMessage;
+        }
+        link.setAttribute('href', href);
+      }
+    });
+
+    console.log("WhatsApp links updated successfully.");
+}
+
+function getThisScriptUrl() {
+  var scripts = document.getElementsByTagName('script');
+  for (var i = 0; i < scripts.length; i++) {
+      if (scripts[i].src.includes('utm_whatsapp.js')) {
+          return scripts[i].src;
+       }
+  }
+  return null;
+}
+
+function getQueryParam(url, param) {
+        // Criar um objeto URL
+        const scriptUrl = new URL(url);
+
+        // Criar um objeto URLSearchParams
+        const searchParams = new URLSearchParams(scriptUrl.search);
+
+        // Retornar o valor do parÃ¢metro
+        return searchParams.get(param);
+    }
+
+
+(function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let clientId;
+    const scriptUrl = getThisScriptUrl();
+    if (scriptUrl) {
+		clientId = getQueryParam(scriptUrl, 'clientId');
+        updateWhatsAppLinks(clientId);
+        console.log('Valor de clientId:', clientId);
+    } else {
+        console.log('Script nÃ£o encontrado.');
+    }
+
+    
+})();
